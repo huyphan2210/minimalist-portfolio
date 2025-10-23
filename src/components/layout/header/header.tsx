be10a0a7ab755d8a7792fca18357a925797378de
Huy Phan <huy.phan2210@outlook.com>
2025-10-23 19:52:06 +0700

    [Header] Somewhat style header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,8 +1,38 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+
 import styles from "./header.module.scss";
 
+import logo from "./../../../icons/logo.svg";
+import menu from "./../../../icons/menu.svg";
+import close from "./../../../icons/close.svg";
+
 const LayoutHeader: FC = () => {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <Link href={"/"} className={styles["header__home-link"]}>
+        <Image
+          className={styles["header__home-link__img"]}
+          src={logo}
+          loading="lazy"
+          alt="Logo"
+        />
+      </Link>
+      <nav className={styles["header__nav"]}>
+        <button type="button" className={styles.header__nav_btn}>
+          <Image src={menu} loading="lazy" alt="Logo" />
+        </button>
+        <button
+          type="button"
+          className={`${styles.header__nav_btn} ${styles["header__nav_btn--non-display"]}`}
+        >
+          <Image src={close} loading="lazy" alt="Logo" />
+        </button>
+      </nav>
+    </header>
+  );
 };
 
 export default LayoutHeader;
