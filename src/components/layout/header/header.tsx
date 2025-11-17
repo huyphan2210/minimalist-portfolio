d997bf4beffcdd2e65e0145cfe098994695db6e5
Huy Phan <huy.phan2210@outlook.com>
2025-11-17 16:00:21 +0700

    Finish header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { headers } from "next/headers";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -7,13 +8,17 @@ import styles from "./header.module.scss";
 
 import logo from "./../../../icons/logo.svg";
 import MobileNavHandler from "./mobile-nav-handler/mobile-nav-handler";
+
 import { ICommonNavigationList } from "@/common/navigationList";
+import { REQUEST_HEADERS } from "@/middleware";
 
 export interface ILayoutHeader {
   navigationList: ICommonNavigationList[];
 }
 
-const LayoutHeader: FC<ILayoutHeader> = ({ navigationList }) => {
+const LayoutHeader: FC<ILayoutHeader> = async ({ navigationList }) => {
+  const headerInfo = await headers();
+  const pathName = headerInfo.get(REQUEST_HEADERS.pathName);
   return (
     <header className={styles.header}>
       <Link href={"/"} className={styles["header__home-link"]}>
@@ -28,7 +33,14 @@ const LayoutHeader: FC<ILayoutHeader> = ({ navigationList }) => {
         <MobileNavHandler />
         <ul className={styles.header__nav__list}>
           {navigationList.map((navigation, index) => (
-            <li key={index}>
+            <li
+              className={`${styles.header__nav__list__item} ${
+                pathName === navigation.url
+                  ? styles["header__nav__list__item--active"]
+                  : ""
+              }`}
+              key={index}
+            >
               <Link href={navigation.url}>{navigation.pageTitle}</Link>
             </li>
           ))}
