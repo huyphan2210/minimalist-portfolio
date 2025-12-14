2b73d70456ed30ebb8fe71e15ef2d886452f8f7f
Huy Phan <huy.phan2210@outlook.com>
2025-12-14 16:44:08 +0700

    [FEAT-Home] Style home page

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,10 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./page.module.scss";
 import HomeServices from "@/services/home.services";
 
-export default async function Home() {
-  const content = await HomeServices.getHomePageData();
+const Home = async () => {
+  const {
+    introHeading,
+    introImage,
+    introCtaUrl,
+    aboutMeTitle,
+    aboutMeDescription,
+    aboutMeImage,
+    aboutMeCtaUrl,
+    contactMeTitle,
+    contactMeCtaUrl,
+  } = await HomeServices.getHomePageData();
+
   return (
     <>
-      <h1>{content.introHeading}</h1>
+      <section className={styles.home__intro}>
+        <Image
+          src={HomeServices.returnMediaFullURL(introImage.url)}
+          loading="lazy"
+          alt="Image - Image"
+          width={introImage.width}
+          height={introImage.height}
+        ></Image>
+        <h1 className={styles.home__intro__heading}>{introHeading}</h1>
+        <Link href={introCtaUrl} title="Call to Action - Intro" />
+      </section>
+      <section>
+        <Image
+          src={HomeServices.returnMediaFullURL(aboutMeImage.url)}
+          loading="lazy"
+          alt="Image - About Me"
+          width={aboutMeImage.width}
+          height={aboutMeImage.height}
+        ></Image>
+        <h2>{aboutMeTitle}</h2>
+        <p>{aboutMeDescription}</p>
+        <Link href={aboutMeCtaUrl} title="Call to Action - Contact Me" />
+      </section>
+      <section>
+        <h2>{contactMeTitle}</h2>
+        <Link href={contactMeCtaUrl} title="Call to Action - Contact Me" />
+      </section>
     </>
   );
-}
+};
+
+export default Home;
